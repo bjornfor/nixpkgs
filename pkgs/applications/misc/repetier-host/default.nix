e179951cda2b56a12c54859c93c936fb4b225f3c
Bjørn Forsman <bjorn.forsman@gmail.com>
2013-05-12 11:48:40 +0200

    WIP: repetier-host: use the (prebuilt) binary distribution
    FIXME: need libGL.so
    
    I'm not familiar enough with mono and assemblies, so I'm putting
    building Repetier-Host from source on the TODO list and just using the
    binary distribution for now.

--- pkgs/applications/misc/repetier-host/default.nix
+++ pkgs/applications/misc/repetier-host/default.nix
@@ -1,32 +1,63 @@
-{ stdenv, fetchgit, mono, libgdiplus, pkgconfig, patchelf, opentk }:
-# gtksharp, pkgconfig
+{ stdenv, fetchurl, mono, libgdiplus, libX11, mesa, patchelf }:  # for building binary distribution
+#{ stdenv, fetchgit, mono, libgdiplus, pkgconfig, patchelf, opentk }:  # for building from source
+
+# NOTE: We use the binary distribution of Repetier-Host until someone figures
+# out how to build it from source.
 
 stdenv.mkDerivation rec {
-  version = "0.85";
+  version = "0.85c";
   name = "repetier-host-${version}";
 
-  # Repetier-Host doesn't seem to put out tarballs, only git is available
-  src = fetchgit {
-    url = "git://github.com/repetier/Repetier-Host.git";
-    rev = "refs/tags/v${version}";
-    #sha256 = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
-    #rev = "05c108534abf10dc2e70258bf960c04047312328";
-    sha256 = "1d38ax4szhjvppymgkbk9x1778w130r9ffgksq8pms4j2xd6z0dw";
+  src = fetchurl {
+    name = "${name}.tar.gz";
+    url = "http://www.repetier.com/?wpdmact=process&did=MzUuaG90bGluaw==";
+    sha256 = "1ny22lf9s3yb9bhx9gdxkflvz6q62crzpvza1x449nr6dc2c9w42";
   };
 
-  buildInputs = [ mono libgdiplus pkgconfig opentk ];
+  phases = [ "unpackPhase" "installPhase" ];
 
-  buildPhase = ''
-    export LD_LIBRARY_PATH=${libgdiplus}/lib
-    # Become case insensitive and map unix/win path separators:
-    export MONO_IOMAP=all
-    #echo "MONO_PATH from env: $(env | grep MONO_PATH)"
-    export MONO_PATH=${opentk}/lib
-    #export PKG_CONFIG_PATH=${opentk}/lib/pkgconfig
-    export XBUILD_LOG_REFERENCE_RESOLVER=1
-    xbuild /verbosity:detailed src/RepetierHost/RepetierHost.sln
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/repetier-host
+    cp -r . $out/repetier-host
+
+    pwd
+    ls -l
+
+    # Make a wrapper that invokes Mono + Repetier-Host
+    cat > $out/bin/repetier-host << EOF
+    #!${stdenv.shell}
+    #export LD_LIBRARY_PATH=${libgdiplus}/lib:${libX11}/lib
+    export LD_LIBRARY_PATH=${libX11}/lib:${mesa}/lib
+    ${mono}/bin/mono $out/repetier-host/RepetierHost.exe
+    EOF
+    chmod a+x $out/bin/repetier-host
   '';
 
+  ## TODO: Build from source
+  #
+  ## Repetier-Host doesn't seem to put out tarballs, only git is available
+  #src = fetchgit {
+  #  url = "git://github.com/repetier/Repetier-Host.git";
+  #  rev = "refs/tags/v${version}";
+  #  #sha256 = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
+  #  #rev = "05c108534abf10dc2e70258bf960c04047312328";
+  #  sha256 = "1d38ax4szhjvppymgkbk9x1778w130r9ffgksq8pms4j2xd6z0dw";
+  #};
+  #
+  #buildInputs = [ mono libgdiplus pkgconfig opentk ];
+  #
+  #buildPhase = ''
+  #  export LD_LIBRARY_PATH=${libgdiplus}/lib
+  #  # Become case insensitive and map unix/win path separators:
+  #  export MONO_IOMAP=all
+  #  #echo "MONO_PATH from env: $(env | grep MONO_PATH)"
+  #  export MONO_PATH=${opentk}/lib
+  #  #export PKG_CONFIG_PATH=${opentk}/lib/pkgconfig
+  #  export XBUILD_LOG_REFERENCE_RESOLVER=1
+  #  xbuild /verbosity:detailed src/RepetierHost/RepetierHost.sln
+  #'';
+  #
   #doCheck = false;
 
   # stripping is supposed to break all mono stuff (is it true?)
