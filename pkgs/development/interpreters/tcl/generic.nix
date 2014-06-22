aa5b37ab486467624994278c434c655a4c63a3cd
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-06-22 16:25:03 +0200

    tcl: fix cross-compilation

--- pkgs/development/interpreters/tcl/generic.nix
+++ pkgs/development/interpreters/tcl/generic.nix
@@ -19,6 +19,15 @@ stdenv.mkDerivation rec {
     ln -s $out/bin/tclsh${release} $out/bin/tclsh
   '';
   
+  crossAttrs = {
+    # Tcl configure script makes some pessimistic assumptions when
+    # cross-compiling (when AC_TRY_RUN can't run). One of them is guessing
+    # libc has no strtod when small test program can't be run. Some AC_TRY_RUN
+    # should actually be replaced by AC_TRY_LINK to provide better estimate.
+    # Work around this by forcing configure detection.
+    configureFlags = "ac_cv_func_strtod=yes tcl_cv_strtod_buggy=1";
+  };
+
   meta = with stdenv.lib; {
     description = "The Tcl scription language";
     homepage = http://www.tcl.tk/;
