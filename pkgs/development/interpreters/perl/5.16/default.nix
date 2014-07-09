e8af3994485631e0797cb2ce6109dce09423744f
Bjørn Forsman <bjorn.forsman@gmail.com>
2014-07-09 17:26:50 +0200

    WIP: perl: fix cross-compilation
    FIXME:
     * perl-cross archive is not fetchable with fetchurlBoot (missing
       SSL). Need to find another mirror.
     * Clean up commented out code.
    
    When cross-compiling, use the perl-cross "overlay" to provide a sane
    (wrt. cross-compilation) configure/build system.

--- pkgs/development/interpreters/perl/5.16/default.nix
+++ pkgs/development/interpreters/perl/5.16/default.nix
@@ -1,8 +1,19 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, which, file }:
 
 let
 
   libc = if stdenv.gcc.libc or null != null then stdenv.gcc.libc else "/usr";
+
+  # perl-cross is an overlay for the perl source tarball that provides a
+  # configure/build system that capable of cross-compilation.
+  perlCross = fetchurl {
+    # GitHub always redirects to HTTPS, which isn't available in fetchurlBoot.
+    # Find another mirror, or do some extra bootstrapping; if we're
+    # cross-compiling perl we have already built a full fetchurl with SSL
+    # support.
+    url = "https://github.com/arsv/perl-cross/raw/releases/perl-5.16.3-cross-0.7.4.tar.gz";
+    sha256 = "1jc7fpdknbblirdc8x2cnb1ka795hlwvvmp5s5ads07jvabjriis";
+  };
 
 in
 
@@ -71,4 +82,49 @@ stdenv.mkDerivation rec {
   setupHook = ./setup-hook.sh;
 
   passthru.libPrefix = "lib/perl5/site_perl";
+
+  crossAttrs = {
+
+    buildInputs = [ which file ];  # perl-cross fails in mysterious ways without 'which'
+
+    postUnpack = ''
+      echo "Applying perl-cross overlay..."
+      tar xvf "${perlCross}" --strip-components=1 -C perl-*
+      patchShebangs .
+    '';
+
+    configurePhase = ''
+      ./configure --target=${stdenv.cross.config} --prefix="$out"
+      # Configure options from Buildroot
+      #  -Dmydomain="" \
+      #  -Dmyhostname="nixpkgs" \
+      #  -Dmyuname="nixpkgs full-version-number" \
+      #  -Dosname=linux \
+      #  -Dosvers=$(LINUX_VERSION) \
+      #  -Dperladmin=root
+    '';
+
+#    configureScript = "./configure";
+#
+#    # FIXME:
+#    #   Checking for linker ... none found
+#    #   ERROR: no linker found
+#    configureFlags =
+#      [ "--target=${stdenv.cross.config}"
+#        "--target-tools-prefix=${stdenv.cross.config}-"  # buildroot full path and railing dash, perl-cross documentation uses just the target triplet + '-'
+#        "--host-cc=gcc"
+#        #"--host-ld=ld"  # invalid flag, trying with --build="" to prevent host tools prefix, but doesn't work :/
+#        "--host-ar=ar"
+#        "--host-ranlib=ranlib"
+#        "--host-objdump=objdump"
+#
+#        "--build="  # no prefix for host tools (DOESN'T WORK!)
+#
+#        #"-Uinstallusrbinperl"
+#        #"-Dinstallstyle=lib/perl5"
+#        #"-Duseshrplib"
+#        #"-Dlocincpth=${libc}/include"
+#        #"-Dloclibpth=${libc}/lib"
+#      ] ++ optional (stdenv ? glibc) "-Dusethreads";
+  };
 }
