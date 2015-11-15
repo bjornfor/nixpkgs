54ce819b4e3e9d463afb646544625a7a3c6adc2c
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-11-15 12:59:19 +0100

    WIP: nixos: add x2goserver module

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -270,6 +270,8 @@
       toxvpn = 247;
       squeezelite = 248;
       turnserver = 249;
+      x2gouser = 250;
+      x2goprint = 251;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -510,6 +512,8 @@
       #toxvpn = 247; # unused
       #squeezelite = 248; #unused
       turnserver = 249;
+      x2gouser = 250;
+      x2goprint = 251;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
---
+++ nixos/modules/services/networking/x2goserver.nix
@@ -0,0 +1,54 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.services.x2goserver;
+
+in
+
+{
+
+  options = {
+
+    services.x2goserver = {
+
+      enable = mkOption {
+        default = false;
+        type = types.bool;
+        description = ''
+          Whether or not to enable the the X2Go remote desktop server.
+        '';
+      };
+
+    };
+
+  };
+
+  config = mkIf cfg.enable {
+
+    environment.systemPackages = [ pkgs.x2goserver ];
+
+    environment.etc."x2gosql/sql".text = ''
+      backend=sqlite
+    '';
+
+    systemd.packages = [ pkgs.x2goserver ];
+
+    users.extraUsers.x2gouser = {
+      group = "x2gouser";
+      description = "X2Go remote desktop user";
+      uid = config.ids.uids.x2gouser;
+      gid = config.ids.gids.x2gouser;
+    };
+
+    users.extraUsers.x2goprint = {
+      group = "x2goprint";
+      description = "X2Go remote desktop print user";
+      uid = config.ids.uids.x2goprint;
+      gid = config.ids.gids.x2goprint;
+    };
+
+  };
+}
