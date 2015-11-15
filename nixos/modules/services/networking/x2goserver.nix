e4dfdb538970217956e70814505f49dc7b4614f8
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-11-15 21:08:28 +0100

    nixos/x2goserver: WIP

--- nixos/modules/services/networking/x2goserver.nix
+++ nixos/modules/services/networking/x2goserver.nix
@@ -35,22 +35,46 @@ in
     '';
 
     systemd.packages = [ pkgs.x2goserver ];
+    systemd.services.x2goserver.preStart = ''
+      # X2go homedir + printing spool dir
+      install -dm 0770 --owner=x2gouser --group=x2gouser /var/lib/x2go
+      install -dm 0770 --owner=x2goprint --group=x2goprint /var/spool/x2goprint
+    '';
+      ## load fuse module at system start
+      #install -dm755 $pkgdir/usr/lib/modules-load.d
+      #echo "fuse" > $pkgdir/usr/lib/modules-load.d/x2goserver.conf
+
+      #install -dm 755 "${pkgdir}/usr/share/doc/${pkgname}"
+      #install -m 644 "ChangeLog" "${pkgdir}/usr/share/doc/${pkgname}/"
+
+      ## fix permission - see INSTALL file
+      #chown root:111 ${pkgdir}/usr/lib/x2go/x2gosqlitewrapper
+      #chmod 2755 ${pkgdir}/usr/lib/x2go/x2gosqlitewrapper
+
+      #chown root:112 ${pkgdir}/usr/bin/x2goprint
+      #chmod 2755 ${pkgdir}/usr/bin/x2goprint
+
+      #chmod 750 ${pkgdir}/etc/sudoers.d
+      #chmod 0440 ${pkgdir}/etc/sudoers.d/x2goserver
 
     users.users.x2gouser = {
       description = "X2Go remote desktop user";
       uid = config.ids.uids.x2gouser;
-      #gid = config.ids.gids.x2gouser;
-      createHome = true;
+      group = "x2gouser";
+      #createHome = true;  # let systemd do it (ensures perms are OK)
       home = "/var/lib/x2go";
     };
 
     users.users.x2goprint = {
       description = "X2Go remote desktop print user";
       uid = config.ids.uids.x2goprint;
-      #gid = config.ids.gids.x2goprint;
-      createHome = true;
+      group = "x2goprint";
+      #createHome = true;  # let systemd do it (ensures perms are OK)
       home = "/var/spool/x2goprint";
     };
+
+    users.groups.x2gouser.gid = config.ids.gids.x2gouser;
+    users.groups.x2goprint.gid = config.ids.gids.x2goprint;
 
   };
 }
