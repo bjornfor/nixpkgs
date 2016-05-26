b96563cc8aa60b9584a93bab5fa3aebf3079248c
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-05-26 16:14:27 +0200

    Revert "WIP: evolution-ews: try building it as part of evolution-data-server"
    This reverts commit 1fa5f43a0b73dd18a287a98737148351fa65c199.
    
    Didn't work (without additional effort). Also not the direction we want
    to go.

--- pkgs/desktops/gnome-3/3.18/core/evolution-data-server/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/evolution-data-server/default.nix
@@ -1,28 +1,14 @@
 { fetchurl, stdenv, pkgconfig, gnome3, python
 , intltool, libsoup, libxml2, libsecret, icu, sqlite
-, p11_kit, db, nspr, nss, libical, gperf, makeWrapper, valaSupport ? true, vala
-, enableEws ? true, /*evolution ? null,*/ webkitgtk ? null, libmspack ? null
-}:
+, p11_kit, db, nspr, nss, libical, gperf, makeWrapper, valaSupport ? true, vala }:
 
-assert enableEws -> /*evolution != null &&*/ webkitgtk != null && libmspack != null;
-
-let
-  ews = rec {
-    name = "evolution-ews-${gnome3.version}.4";
-    src = fetchurl {
-      url = "mirror://gnome/sources/evolution-ews/${gnome3.version}/${name}.tar.xz";
-      sha256 = "01y706dfi95dfiwnsyba35iz921b5j3qmwvmmjj2abn3y7qx91cj";
-    };
-  };
-in
 stdenv.mkDerivation rec {
   inherit (import ./src.nix fetchurl) name src;
 
   buildInputs = with gnome3;
     [ pkgconfig glib python intltool libsoup libxml2 gtk gnome_online_accounts
       gcr p11_kit libgweather libgdata gperf makeWrapper icu sqlite gsettings_desktop_schemas ]
-    ++ stdenv.lib.optional valaSupport vala
-    ++ stdenv.lib.optionals enableEws [ /*webkitgtk*/ libmspack ];
+    ++ stdenv.lib.optional valaSupport vala;
 
   propagatedBuildInputs = [ libsecret nss nspr libical db ];
 
@@ -34,15 +20,6 @@ stdenv.mkDerivation rec {
     for f in "$out/libexec/"*; do
       wrapProgram "$f" --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
     done
-  '';
-
-  # EWS really wants to be inside evolution_data_server, so let it. (It's not
-  # that big.)
-  postInstall = stdenv.lib.optional enableEws ''
-    echo "Building EWS"
-    tar xvf ${ews.src}
-    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$out/lib/pkgconfig"
-    (cd ${ews.name} && ./configure --prefix=$out && make && make install)
   '';
 
   meta = with stdenv.lib; {
