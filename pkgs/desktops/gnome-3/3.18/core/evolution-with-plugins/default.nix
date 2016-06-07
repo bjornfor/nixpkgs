201b3cff6653b4f263b14019786208febffa6e56
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-06-07 19:56:39 +0200

    WIP: evolution-with-plugins: attempt to fix ELF files with bbe replacer (doesn't work)

--- pkgs/desktops/gnome-3/3.18/core/evolution-with-plugins/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/evolution-with-plugins/default.nix
@@ -24,15 +24,32 @@ runCommand "evolution-with-plugins" {} ''
   #        ${stdenv.lib.concatMapStringsSep " " (x: "-e \"s|${x}|$out|\"") plugins} \
   #        '{}' -o '{}' \;
 
+  ## The perl replacer messes up ELF files.
+  #cat > perl-replacer << __EOF__
+  ##!${stdenv.shell}
+  #set -x
+  #i=\$1
+  #cat "\$i" \
+  #    | ${perl}/bin/perl -pe "s|${evolution}|$out|g" \
+  #    | ${perl}/bin/perl -pe "s|${evolution_data_server}|$out|g" \
+  #    | ${stdenv.lib.concatMapStringsSep " | " (x: "${perl}/bin/perl -pe \"s|${x}|$out|g\"") plugins} \
+  #    > "\$i.tmp"
+  #if test -x "\$i"; then chmod +x "\$i.tmp"; fi
+  #mv "\$i.tmp" "\$i"
+  #__EOF__
+  #chmod +x perl-replacer
+  #find "$out" -type f -exec ./perl-replacer '{}' \;
+
+  # The bbe replacer messes up ELF files too.
   cat > replacer << __EOF__
   #!${stdenv.shell}
   set -x
   i=\$1
-  cat "\$i" \
-      | ${perl}/bin/perl -pe "s|${evolution}|$out|g" \
-      | ${perl}/bin/perl -pe "s|${evolution_data_server}|$out|g" \
-      | ${stdenv.lib.concatMapStringsSep " | " (x: "${perl}/bin/perl -pe \"s|${x}|$out|g\"") plugins} \
-      > "\$i.tmp"
+  ${bbe}/bin/bbe \
+      -e "s|${evolution}|$out|" \
+      -e "s|${evolution_data_server}|$out|" \
+      ${stdenv.lib.concatMapStringsSep " " (x: "-e \"s|${x}|$out|\"") plugins} \
+      "\$i" -o "\$i.tmp"
   if test -x "\$i"; then chmod +x "\$i.tmp"; fi
   mv "\$i.tmp" "\$i"
   __EOF__
