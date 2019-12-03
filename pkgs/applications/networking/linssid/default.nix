83943dbcb22de4486c622c7a549c3556d413f393
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-12-03 23:37:19 +0100

    WIP: linssid: 2.7 -> 3.6
    FIXME: There is a new pkexec launcher that hardcodes /usr path. Suggest
    to upstream to require a little setuid wrapper like wireshark (seeing as
    upstream claims ubuntu crippled sudo/su and forced the use of pkexec).
    
    FIXME: Qt wrapping not working.

--- pkgs/applications/networking/linssid/default.nix
+++ pkgs/applications/networking/linssid/default.nix
@@ -1,33 +1,37 @@
 { lib, stdenv, fetchurl, qtbase, qtsvg, qmake, pkg-config, boost, wirelesstools, iw, qwt6_1, wrapQtAppsHook }:
 
-stdenv.mkDerivation rec {
+mkDerivation rec {
   pname = "linssid";
-  version = "2.7";
+  version = "3.6";
 
   src = fetchurl {
     url = "mirror://sourceforge/project/linssid/LinSSID_${version}/linssid_${version}.orig.tar.gz";
-    sha256 = "13d35rlcjncd8lx3khkgn9x8is2xjd5fp6ns5xsn3w6l4xj9b4gl";
+    sha256 = "1774wcr90jk0zil3psd545zz60l5f57bys366492b3vh7zliwc2p";
   };
+  #src = ../../../../linssid-3.6/.;
 
   nativeBuildInputs = [ pkg-config qmake wrapQtAppsHook ];
   buildInputs = [ qtbase qtsvg boost qwt6_1 ];
 
   patches = [ ./0001-unbundled-qwt.patch ];
 
-  postPatch = ''
-    sed -e "s|/usr/include/qt5.*$|& ${qwt6_1}/include|" -i linssid-app/linssid-app.pro
-    sed -e "s|/usr/include/|/nonexistent/|g" -i linssid-app/*.pro
-    sed -e 's|^LIBS .*= .*libboost_regex.a|LIBS += -lboost_regex|' \
-        -e "s|/usr|$out|g" \
-        -i linssid-app/linssid-app.pro linssid-app/linssid.desktop
-    sed -e "s|\.\./\.\./\.\./\.\./usr|$out|g" -i linssid-app/*.ui
+  #qmakeFlags = [ "PREFIX=/foobar" ];
+  postUnpack = "set -x";
 
-    # Remove bundled qwt
-    rm -fr qwt-lib
-  '';
+  #postPatch = ''
+  #  sed -e "s|/usr/include/qt5.*$|& ${qwt}/include|" -i linssid-app/linssid-app.pro
+  #  sed -e "s|/usr/include/|/nonexistent/|g" -i linssid-app/*.pro
+  #  sed -e 's|^LIBS .*= .*libboost_regex.a|LIBS += -lboost_regex|' \
+  #      -e "s|/usr|$out|g" \
+  #      -i linssid-app/linssid-app.pro linssid-app/linssid.desktop
+  #  sed -e "s|\.\./\.\./\.\./\.\./usr|$out|g" -i linssid-app/*.ui
 
-  qtWrapperArgs =
-    [ ''--prefix PATH : ${lib.makeBinPath [ wirelesstools iw ]}'' ];
+  #  # Remove bundled qwt
+  #  rm -fr qwt-lib
+  #'';
+
+  #qtWrapperArgs =
+  #  [ ''--prefix PATH : ${lib.makeBinPath [ wirelesstools iw ]}'' ];
 
   meta = with lib; {
     description = "Graphical wireless scanning for Linux";
