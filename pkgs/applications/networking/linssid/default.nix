7a532a4a499970d6d43dcb0b641ca5d31acd0123
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-12-04 16:57:13 +0100

    WIP: linssid: found the problem with wrapQt* not wrapping
    It only looks in ./bin, whereas linssid is installed in ./sbin. Fix in
    nixpkgs.

--- pkgs/applications/networking/linssid/default.nix
+++ pkgs/applications/networking/linssid/default.nix
@@ -1,22 +1,26 @@
 { lib, stdenv, fetchurl, qtbase, qtsvg, qmake, pkg-config, boost, wirelesstools, iw, qwt6_1, wrapQtAppsHook }:
 
-mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "linssid";
   version = "3.6";
 
-  src = fetchurl {
+  src2 = fetchurl {
     url = "mirror://sourceforge/project/linssid/LinSSID_${version}/linssid_${version}.orig.tar.gz";
     sha256 = "1774wcr90jk0zil3psd545zz60l5f57bys366492b3vh7zliwc2p";
   };
-  #src = ../../../../linssid-3.6/.;
+  src = ../../../../linssid-3.6/.;
 
   nativeBuildInputs = [ pkg-config qmake wrapQtAppsHook ];
   buildInputs = [ qtbase qtsvg boost qwt6_1 ];
 
-  patches = [ ./0001-unbundled-qwt.patch ];
+  #patches = [ ./0001-unbundled-qwt.patch ];
 
   #qmakeFlags = [ "PREFIX=/foobar" ];
   postUnpack = "set -x";
+  preFixup = ''
+    find "$out"
+    ls -l "$out/bin/linssid" || true
+  '';
 
   #postPatch = ''
   #  sed -e "s|/usr/include/qt5.*$|& ${qwt}/include|" -i linssid-app/linssid-app.pro
